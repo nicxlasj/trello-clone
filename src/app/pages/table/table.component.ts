152ae2b7403965a2bfba9cc1a5e1f8e474972353
nico <nicolasjaiquel@gmail.com>
2024-03-31 14:15:30 -0500

    Table

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -2,24 +2,9 @@ import { Component, OnInit, inject, signal } from '@angular/core';
 import { CdkTableModule, DataSource } from '@angular/cdk/table';
 import { ProductsService } from '../../services/products.service';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { Product } from '../../models/Product';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { CollectionViewer } from '@angular/cdk/collections';
 
-var products = signal<Product[]>([{
-  id : 1,
-  title : 'Playstation 5',
-  description : 'The best videogames console',
-  images : [''],
-  price : 500
-},
-{
-  id : 2,
-  title : 'Xbox series X',
-  description : 'A good videogames console :)',
-  images : [''],
-  price : 500
-}]);
+
+// var products = signal<Product[]>([]);
 
 @Component({
   selector: 'app-table',
@@ -31,20 +16,20 @@ var products = signal<Product[]>([{
 
 export class TableComponent implements OnInit {
   private _productService = inject(ProductsService);
-  displayedColumns : string[] = ['title', 'description', 'price']
-  
+  displayedColumns : string[] = ['title', 'cover', 'price']
+  products2 = this._productService.products
   ngOnInit(): void {
     this._productService.getProducts();
   }
-  dataSource = new ProductsDataSource();
+  // dataSource = new ProductsDataSource();
 }
 
-export class ProductsDataSource extends DataSource<Product> {
-  data = new BehaviorSubject<Product[]>(products());
-  connect() : Observable<Product[]> {
-      return this.data;
-  }
-  override disconnect(collectionViewer: CollectionViewer): void {
+// export class ProductsDataSource extends DataSource<Product> {
+//   data = new BehaviorSubject<Product[]>(products());
+//   connect() : Observable<Product[]> {
+//       return this.data;
+//   }
+//   override disconnect(collectionViewer: CollectionViewer): void {
     
-  }
-}
+//   }
+// }
